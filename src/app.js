ebb3c1abce514a3715b0b6d5a32848927489f550
Phong Luu <huynhpluu@gmail.com>
2020-12-17 22:06:43 +0700

    config: routes

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const router = require("./routes");
+
 const app = express();
 
 app.set("views", "src/views");
 app.set("view engine", "pug");
 
-app.get("/", function (req, res) {
-  res.render("index", { title: "Hey", message: "Hello there!" });
-});
+app.use(cors());
+
+app.use(
+  bodyParser.json({
+    limit: "50mb",
+  })
+);
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+
+app.use("/", router);
 
 module.exports = app;
